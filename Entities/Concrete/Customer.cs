67b1932e3a5a72bc1ddadae5a8af309d23924928
MesutRuzgar <mesutruzgar@gmail.com>
2023-03-09 21:17:12 +0300

    Add project files.

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Customer : IEntity
+    {
+        public int Id { get; set; }
+        public string TaxIdentificationNumber { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string TaxPlateYear { get; set; }
+        public DateTime ActivityCertificateDate { get; set; }
+        public DateTime SignatureCircularDate { get; set; }
+        public string CompanyOfficials { get; set; }
+    }
+}
